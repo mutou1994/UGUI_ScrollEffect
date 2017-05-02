7ce225ecf3f150dafacf3053f081ca9d02b9dc8f
Mot <304469594@qq.com>
2017-05-02 15:39:34 +0800

    first

---
+++ Assets/CenterOnChild.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+public class CenterOnChild : MonoBehaviour, IEndDragHandler, IBeginDragHandler
+{
+    public Action<GameObject> onCenterCallBack;
+    public Action<GameObject> onCenterFinshed;
+    public GameObject centeredObject { get; private set; }
+    public CenterMode centerMode = CenterMode.centerOnChild;
+    public float speed = 10;
+    ScrollRect scroll;
+    GridLayoutGroup grid;
+    Transform content;
+    void Awake()
+    {
+        scroll = GetComponentInParent<ScrollRect>();
+        content = scroll.content;
+        grid = content.GetComponent<GridLayoutGroup>();
+    }
+    void Start()
+    {
+        //考虑到Grid排列会先打乱Item的坐标,这回导致寻找最靠近中心的Item与计算偏移量出错，所以等排列完成再居中
+        Invoke("reCenter", 0.05f);
+    }
+    Vector3 CalPickingPoint()
+    {
+        Vector3 pickingPoint = Vector3.zero;
+        //裁剪区域四角坐标
+        Vector2 halfSize = (scroll.transform as RectTransform).rect.size / 2;
+        Vector2[] Conners = new Vector2[4];
+        Conners[0] = new Vector2(-halfSize.x, halfSize.y);
+        Conners[1] = new Vector2(halfSize.x, halfSize.y);
+        Conners[2] = new Vector2(halfSize.x, -halfSize.y);
+        Conners[3] = new Vector2(-halfSize.x, -halfSize.y);
+
+        if (centerMode == CenterMode.centerOnChild)
+        {
+            for (int i = 0; i < 4; i++)
+                Conners[i] = scroll.transform.TransformPoint(Conners[i]);
+            pickingPoint = (Conners[0] + Conners[2]) * 0.5f;
+        }
+        else
+        {
+            RectTransform item = content.GetChild(0) as RectTransform;
+            Vector2 half_itemSize = item.rect.size * 0.5f;
+            half_itemSize.y *= -1;
+            pickingPoint = scroll.transform.TransformPoint(Conners[0] + half_itemSize);
+        //    Debug.Log(pickingPoint);
+        }
+        return pickingPoint;
+    }
+    public void reCenter()
+    {
+        Vector3 pickingPoint = CalPickingPoint();
+        float minDist = float.MaxValue;
+        Transform closest = null;
+        for (int i = 0, Imax = content.childCount; i < Imax; i++)
+        {
+            Transform child = content.GetChild(i);
+            if (!child.gameObject.activeInHierarchy) continue;
+            float dist = Vector2.SqrMagnitude(child.position - pickingPoint);
+
+            if (dist < minDist)
+            {
+
+                minDist = dist;
+                closest = child;
+            }
+
+        }
+        CenterOn(closest, pickingPoint);
+    }
+    void CenterOn(Transform target, Vector3 centerPos)
+    {
+        if (target != null && scroll != null)
+        {
+            centeredObject = target.gameObject;
+            Vector3 cp = content.parent.InverseTransformPoint(target.position);
+            Vector3 cc = content.parent.InverseTransformPoint(centerPos);
+            Vector3 localOffset = cc - cp;
+            if (!scroll.horizontal) localOffset.x = 0;
+            if (!scroll.vertical) localOffset.y = 0;
+            localOffset.z = 0;
+            targetPos = content.localPosition + localOffset;
+            centering = true;
+            if (onCenterCallBack != null) onCenterCallBack(target.gameObject);
+        }
+    }
+    public void CenterOn(Transform target)
+    {
+        CenterOn(target, CalPickingPoint());
+    }
+    Vector3 targetPos;
+    bool centering = false;
+    void Update()
+    {
+        if (centering)
+        {
+            Vector2 v = content.localPosition;
+            content.localPosition = Vector2.Lerp(content.localPosition, targetPos, speed * Time.deltaTime);
+            if (Vector2.Distance(content.localPosition, targetPos) < 0.01f)
+            {
+                content.localPosition = targetPos;
+                centering = false;
+                if (onCenterFinshed != null) onCenterFinshed(centeredObject);
+            }
+        }
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        scroll.StopMovement();
+        reCenter();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        centering = false;
+        targetPos = Vector3.zero;
+    }
+    public enum CenterMode
+    {
+        centerOnChild,
+        centerOnItem,
+    }
+}
+
---
+++ Assets/ScrollRectEffect.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ScrollRectEffect : MonoBehaviour
+{
+
+    float left_alpha = 0.6f;
+    float right_alpha = 0.4f;
+    float middle_alpha = 1f;
+
+    float left_height = 150;
+    float right_height = 100;
+    float middle_height = -120;
+
+    float left_scale = 0.6f;
+    float right_scale = 0.3f;
+    float middle_scale = 1f;
+
+    ScrollRect scroll;
+    List<Transform> contents;
+    Vector2 centerPoint;
+    Vector2 viewPort_size;
+
+    void Awake()
+    {
+        scroll = transform.GetComponentInParent<ScrollRect>();
+        CacheContents();
+        CalCenterPoint();
+        scroll.onValueChanged.AddListener(onScroll);
+    }
+
+    void CacheContents()
+    {
+        if (contents == null)
+            contents = new List<Transform>();
+        contents.Clear();
+        for (int i = 0, Imax = transform.childCount; i < Imax; i++)
+        {
+            Transform child = transform.GetChild(i).Find("content");
+            contents.Add(child);
+        }
+    }
+
+    void CalCenterPoint()
+    {
+        viewPort_size = (scroll.transform as RectTransform).rect.size;
+
+        Vector3 pickingPoint = Vector3.zero;
+        //裁剪区域四角坐标
+        Vector2 halfSize = viewPort_size / 2;
+        Vector2[] Conners = new Vector2[4];
+        Conners[0] = new Vector2(-halfSize.x, halfSize.y);
+        Conners[1] = new Vector2(halfSize.x, halfSize.y);
+        Conners[2] = new Vector2(halfSize.x, -halfSize.y);
+        Conners[3] = new Vector2(-halfSize.x, -halfSize.y);
+
+        centerPoint = (Conners[0] + Conners[2]) * 0.5f;
+    }
+
+    void onScroll(Vector2 vec)
+    {
+        Adjust();
+    }
+
+    void Adjust()
+    {
+        CalCenterPoint();
+        for (int i = 0, Imax = contents.Count; i < Imax; i++)
+        {
+            Vector3 pos = scroll.transform.InverseTransformPoint(contents[i].position);
+            if (!scroll.horizontal) pos.x = centerPoint.x;
+            if (!scroll.vertical) pos.y = centerPoint.y;
+            pos.z = 0;
+            float dis = Vector3.Distance(centerPoint, pos);
+            int direct = pos.x < centerPoint.x ? -1 : 1;
+
+            AdjustAlpha(i, dis, direct < 0 ? left_alpha : right_alpha);
+            AdjustScale(i, dis, direct < 0 ? left_scale : right_scale);
+            AdjustPosition(i, dis, direct < 0 ? left_height : right_height);
+        }
+    }
+
+    void AdjustScale(int index, float dis, float minVal)
+    {
+        float rate = 1 - (dis / (viewPort_size.x / 2f));
+        float scale = Mathf.Lerp(minVal, middle_scale, rate);
+        Vector3 vec = Vector3.zero;
+        vec.x = vec.y = vec.z = scale;
+        contents[index].localScale = vec;
+    }
+
+    void AdjustAlpha(int index, float dis, float minVal)
+    {
+        float rate = 1 - (dis / (viewPort_size.x / 2f));
+        float alpha = Mathf.Lerp(minVal, middle_alpha, rate);
+        var canvas = contents[index].GetComponent<CanvasGroup>();
+        canvas.alpha = alpha;
+    }
+
+    void AdjustPosition(int index, float dis, float minVal)
+    {
+        float height = Mathf.Lerp(minVal, middle_height, 1 - (dis / (viewPort_size.x / 2f)));
+        Vector3 pos = contents[index].localPosition;
+        pos.y = height;
+        contents[index].localPosition = pos;
+
+    }
+}
+
